4010478112ad4c63be15b270a62da39efda629e2
Quentin Holness <Quentin Holness>
2017-04-12 21:48:22 -0600

    update keys

--- MiniMaps/MiniMaps/__init__.py
+++ MiniMaps/MiniMaps/__init__.py
@@ -1,11 +1,11 @@
-import os
 from flask import Flask
-# import flask_migrate
-# import flask_sqlalchemy
+import flask_migrate
+import flask_sqlalchemy
 
 MinimalMaps = Flask(__name__) # Create MinimalMapslication instance
 MinimalMaps.config.from_object(__name__)
 MinimalMaps.config.update(dict(
-    DATABASE=os.path.join(MinimalMaps.root_path + '/data/database.db')
+    DATABASE=os.path.join(MinimalMaps.root_path + '/data/database.db'),
+    SECRET_KEY='tempDevConfig',
     )
 )
